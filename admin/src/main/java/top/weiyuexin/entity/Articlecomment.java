a2f4b4c5276eeabf5704e4ea24922ad8eb1d9782
LWP123456789 <LWP13226968149@163.com>
2022-05-24 11:07:09 +0800

    entity modify

--- admin/src/main/java/top/weiyuexin/entity/Articlecomment.java
+++ admin/src/main/java/top/weiyuexin/entity/Articlecomment.java
@@ -1,65 +1,20 @@
 package top.weiyuexin.entity;
 
-import java.util.Date;
+import lombok.Data;
 
-public class Articlecomment {
+import java.util.Date;
+@Data
+public class ArticleComment {
     private Integer id;
 
-    private Integer articleid;
+    private Integer articleId;
 
     private String content;
 
-    private Integer authorid;
+    private Integer authorId;
 
     private Date time;
 
     private Integer star;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getArticleid() {
-        return articleid;
-    }
-
-    public void setArticleid(Integer articleid) {
-        this.articleid = articleid;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Integer getAuthorid() {
-        return authorid;
-    }
-
-    public void setAuthorid(Integer authorid) {
-        this.authorid = authorid;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
-
-    public Integer getStar() {
-        return star;
-    }
-
-    public void setStar(Integer star) {
-        this.star = star;
-    }
 }
